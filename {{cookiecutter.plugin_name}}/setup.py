2043fc1ff843269a44571a861f33ec68ddf0b616
Leopold Talirz <leopold.talirz@gmail.com>
2018-06-06 23:36:55 +0200

    change default version to 0.1.0a0
    closer to current pypi conventions

--- {{cookiecutter.plugin_name}}/setup.py
+++ {{cookiecutter.plugin_name}}/setup.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     with open('setup.json', 'r') as info:
         kwargs = json.load(info)
     setup(
-       packages=find_packages(), 
-       long_description=open('README.md').read(),
-       long_description_content_type='text/markdown', 
-       **kwargs)
+        packages=find_packages(),
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        **kwargs)
