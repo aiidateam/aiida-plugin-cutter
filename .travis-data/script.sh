5dace1399fffd6c6464c520972753ed4f65cc64c
Chris Sewell <chrisj_sewell@hotmail.com>
2018-08-28 22:50:11 +0100

    move into directory before installing

--- .travis-data/script.sh
+++ .travis-data/script.sh
@@ -2,15 +2,13 @@
 set -ex
 
 if [ "$TEST_TYPE" == "docs" ] ; then 
-    cd $PLUGIN_DIR/docs
+    cd docs
     make
 elif [ "$TEST_TYPE" == "pre-commit" ] ; then 
-    cd $PLUGIN_DIR
     git init
     git add -A
     pre-commit install
     pre-commit run --all-files || ( git status --short ; git diff ; exit 1 )
 else
-    cd $PLUGIN_DIR
     pytest -v
 fi
