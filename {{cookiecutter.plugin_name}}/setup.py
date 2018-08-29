8b3811206c6b3c5d4ef9e49cf9a7524a50cc0ce9
Chris Sewell <chrisj_sewell@hotmail.com>
2018-08-29 08:49:02 +0100

    Revert "try removing package data"
    This reverts commit 73e7a8985e3fb550d9d6f2248b2a57897dbe750d.

--- {{cookiecutter.plugin_name}}/setup.py
+++ {{cookiecutter.plugin_name}}/setup.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     setup(
         packages=find_packages(),
         # this doesn't work when placed in setup.json (something to do with str type)
-        # package_data={
-        #     "": ["*"],
-        #     # TODO be more specific with package data (but the line below isn't working)
-        #     # "tests.input_files": ["*"],
-        # },
+        package_data={
+            "": ["*"],
+            # TODO be more specific with package data (but the line below isn't working)
+            # "tests.input_files": ["*"],
+        },
         long_description=open('README.md').read(),
         long_description_content_type='text/markdown',
         **kwargs)
