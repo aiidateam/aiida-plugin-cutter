d048a61c6158e4e8d553d9f85fb4a21f2be9e5aa
Leopold Talirz <leopold.talirz@gmail.com>
2018-09-11 14:00:27 +0200

    remove python hooks
    seems like python hook was hiding bash hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 if [ -x "$(command -v yapf)" ]; then
-    echo "Running yapf..."
+    echo "Running yapf on {{ cookiecutter.plugin_name }}"
     yapf -i -r '../{{ cookiecutter.plugin_name }}/'
 else
     echo "yapf not found. 'pip install yapf' to automatically \
-          run formatter on generated plugin"
+          run formatter on {{ cookiecutter.plugin_name }}"
 fi
+
+cat <<EOF
+\n### IMPORTANT###
+Register your plugin NOW by making a pull request to the AiiDA Plugin Registry
+EOF
