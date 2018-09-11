cf6b6262d43631c66a22c9bef87b6bde02bd2d25
Leopold Talirz <leopold.talirz@gmail.com>
2018-09-11 12:19:32 +0200

    add else to yapf check

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 if [ -x "$(command -v yapf)" ]; then
-    echo "Running yapf formatter..."
+    echo "Running yapf..."
     yapf -i -r '../{{ cookiecutter.plugin_name }}/'
+else
+    echo "yapf not found. 'pip install yapf' to automatically \
+          run formatter on generated plugin"
 fi
