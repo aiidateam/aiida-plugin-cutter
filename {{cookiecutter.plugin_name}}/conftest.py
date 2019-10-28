374a839e9afb2c52660fbb2af5b70d446209ce09
Leopold Talirz <leopold.talirz@gmail.com>
2019-10-28 09:36:07 +0100

    use new pytest fixtures

--- {{cookiecutter.plugin_name}}/conftest.py
+++ {{cookiecutter.plugin_name}}/conftest.py
@@ -1,60 +1,15 @@
-"""
-For pytest 
-initialise a text database and profile
-"""
+"""pytest fixtures for simplified testing."""
 from __future__ import absolute_import
-import tempfile
-import shutil
 import pytest
-
-from aiida.manage.fixtures import fixture_manager
-
-
-@pytest.fixture(scope='session', autouse=True)
-def aiida_profile():
-    """Set up a test profile for the duration of the tests"""
-    with fixture_manager() as fixture_mgr:
-        yield fixture_mgr
-
+pytest_plugins = ['aiida.manage.tests.pytest_fixtures']
 
 @pytest.fixture(scope='function', autouse=True)
-def clear_database(aiida_profile):
-    """Clear the database after each test"""
-    yield
-    aiida_profile.reset_db()
-
+def clear_database_auto(clear_database):  # pylint: disable=unused-argument 
+   """Automatically clear database in between tests."""
 
 @pytest.fixture(scope='function')
-def new_workdir():
-    """Get a temporary folder to use as the computer's work directory."""
-    dirpath = tempfile.mkdtemp()
-    yield dirpath
-    shutil.rmtree(dirpath)
-
-
-@pytest.fixture(scope='function')
-def aiida_localhost_computer(new_workdir):
-    """Get an AiiDA computer for localhost.
-
-    :return: The computer node
-    :rtype: :py:class:`aiida.orm.Computer`
+def {{cookiecutter.entry_point_prefix}}_code(aiida_local_code_factory):
+    """Get a {{cookiecutter.entry_point_prefix}} code.
     """
-    from {{cookiecutter.module_name}}.helpers import get_computer
-
-    computer = get_computer(workdir=new_workdir)
-
-    return computer
-
-
-@pytest.fixture(scope='function')
-def aiida_code(aiida_localhost_computer):
-    """Get an AiiDA code.
-
-    :return: The code node
-    :rtype: :py:class:`aiida.orm.Code`
-    """
-    from {{cookiecutter.module_name}}.helpers import get_code
-
-    code = get_code(entry_point='{{cookiecutter.entry_point_prefix}}', computer=aiida_localhost_computer)
-
-    return code
+    {{cookiecutter.entry_point_prefix}}_code = aiida_local_code_factory('diff', '{{cookiecutter.entry_point_prefix}}')
+    return {{cookiecutter.entry_point_prefix}}_code
