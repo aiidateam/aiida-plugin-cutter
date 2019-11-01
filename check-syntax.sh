4df46ae17969e371417253f0b351e88ebfc693e6
Leopold Talirz <leopold.talirz@gmail.com>
2019-11-01 13:39:05 +0100

    update pre-commit hooks (#63)
    * move yapf to virtualenv
    * update version of modernizer
    * update aiida-diff version to 1.0.0
    * fix check-syntax.sh script (would not delete files if removed from package)

--- check-syntax.sh
+++ check-syntax.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-cookiecutter --no-input -f . version=1.0.0b1
+# clean directory contents if exist (but don't delete .git directory)
+if [ -d aiida-diff ]; then
+  rm -r aiida-diff/*
+fi
+
+cookiecutter --no-input -f . version=1.0.0
 pip install -e aiida-diff[docs,pre-commit,testing]
 cd aiida-diff
 git init && git add -A
